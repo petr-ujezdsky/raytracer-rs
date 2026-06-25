735204029e8c144c8bfcb2f3d8ca30e8732f264a
Petr Újezdský <petr.ujezdsky@gmail.com>
2026-06-26 00:55:05 +0200

    Camera implementation

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,11 @@ fn main() {
     let viewport_height = 2.0;
     // Viewport widths less than one are ok since they are real valued.
     let viewport_width = viewport_height * ((image_width as f64) / image_height as f64);
-    let camera_center = Point3 { x: 0.0, y: 0.0, z: 0.0 };
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    let viewport_u = Vec3 { x: viewport_width, y: 0.0, z: 0.0 };
-    let viewport_v = Vec3 { x: 0.0, y: -viewport_height, z: 0.0 };
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
     let pixel_delta_u = viewport_u / image_width as f64;
@@ -37,7 +37,7 @@ fn main() {
 
     // Calculate the location of the upper left pixel.
     let viewport_upper_left = camera_center
-        - Vec3 { x:0.0, y:0.0, z:focal_length } - viewport_u/2.0 - viewport_v/2.0;
+        - Vec3::new(0.0, 0.0, focal_length) - viewport_u/2.0 - viewport_v/2.0;
 
     let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
@@ -95,7 +95,7 @@ fn write_to_file(width: u32, height: u32) {
             let g = j as f64 / (height - 1) as f64;
             let b = 0f64;
 
-            let pixel_color = Color { x: r, y: g, z: b };
+            let pixel_color = Color::new(r, g, b);
             write_color(&mut writer, pixel_color);
         }
     }
