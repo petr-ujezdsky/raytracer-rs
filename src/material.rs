ee3259f67ba9afd04223ec4439e1c90b470efaaf
Petr Újezdský <petr.ujezdsky@gmail.com>
2026-06-27 23:19:43 +0200

    Implemented fuzziness in metal

--- src/material.rs
+++ src/material.rs
@@ -42,21 +42,30 @@ impl Material for Lambertian {
 
 pub struct Metal {
     pub albedo: Color,
+    pub fuzz: f64,
 }
 
 impl Metal {
-    pub(crate) fn new(albedo: Color) -> Metal {
-        Metal { albedo }
+    pub(crate) fn new(albedo: Color, fuzz: f64) -> Metal {
+        Metal { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, r_in: Ray, rec: &HitRecord, rng: &mut Random) -> Option<ScatterRecord> {
-        let reflected = Vec3::reflect(r_in.direction, rec.normal);
+        let mut reflected = Vec3::reflect(r_in.direction, rec.normal);
+
+        reflected = reflected.unit_vector() + (self.fuzz * Vec3::random_unit_vector(rng));
+
+        let scattered = Ray::new(rec.p, reflected);
+
+        if scattered.direction.dot(rec.normal) <= 0.0 {
+            return None;
+        }
 
         Some(ScatterRecord {
             attenuation: self.albedo,
-            scattered: Ray::new(rec.p, reflected),
+            scattered,
         })
     }
 }
