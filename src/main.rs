f2f356c70ac59ec41a54b07008bb8db345f3fa11
Petr Újezdský <petr.ujezdsky@gmail.com>
2026-07-15 18:08:39 +0200

    Final scene

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ mod quad;
 mod constant_medium;
 
 fn main() {
-    match 8 {
+    match 9 {
         0 => three_spheres(),
         1 => bouncing_spheres(),
         2 => checkered_spheres(),
@@ -43,6 +43,8 @@ fn main() {
         6 => simple_light(),
         7 => cornell_box(),
         8 => cornell_smoke(),
+        9 => final_scene(800, 1000, 40),
+        99 => final_scene(400, 250, 4),
         _ => panic!("invalid scene number"),
     }
 }
@@ -465,6 +467,100 @@ fn cornell_smoke() {
 
         vfov: 40,
         lookfrom: Point3::new(278.0, 278.0, -800.0),
+        lookat: Point3::new(278.0, 278.0, 0.0),
+        vup: Vec3::new(0.0, 1.0, 0.0),
+
+        defocus_angle: 0.0,
+        // focus_dist: 10.0,
+
+        rng_seed,
+        ..Default::default()
+    });
+
+    camera.render(&world);
+}
+
+fn final_scene(image_width: u32, samples_per_pixel: u32, max_depth: u32) {
+    // Rng
+    // let rng_seed: Option<u64> = None;
+    let rng_seed = Some(12487324);
+    let mut rng = Random::from_os_or_seeded(rng_seed);
+
+    // Boxes
+    let mut boxes1 = HittableList::default();
+    let ground = Arc::new(Lambertian::from_color(Color::new(0.48, 0.83, 0.53)));
+
+    let boxes_per_side = 20;
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = rng.range(1.0..101.0);
+            let z1 = z0 + w;
+
+            boxes1.add(Quad::create_box(Point3::new(x0, y0, z0), Point3::new(x1, y1, z1), ground.clone()));
+        }
+    }
+
+    // World
+    let mut world = HittableList::default();
+
+    // Add boxes ground
+    world.add(boxes1);
+
+    // Light
+    let light = Arc::new(DiffuseLight::from_color(Color::new(7.0, 7.0, 7.0)));
+    world.add(Quad::new(Point3::new(123.0, 554.0, 147.0), Vec3::new(300.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 265.0), light.clone()));
+
+    // Moving sphere
+    let center1 = Point3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let sphere_material = Arc::new(Lambertian::from_color(Color::new(0.7, 0.3, 0.1)));
+    world.add(Sphere::new_moving(center1, center2, 50.0, sphere_material));
+
+    // Other spheres
+    world.add(Sphere::new(Point3::new(260.0, 150.0, 45.0), 50.0, Arc::new(Dielectric::new(1.5))));
+    world.add(Sphere::new(Point3::new(0.0, 150.0, 145.0), 50.0, Arc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.0))));
+
+    let boundary1 = Arc::new(Sphere::new(Point3::new(360.0, 150.0, 145.0), 70.0, Arc::new(Dielectric::new(1.5))));
+    world.add_arc(boundary1.clone());
+    world.add(ConstantMedium::from_color(boundary1.clone(), 0.2, Color::new(0.2, 0.4, 0.9)));
+
+    let boundary2 = Arc::new(Sphere::new(Point3::new(0.0, 0.0, 0.0), 5000.0, Arc::new(Dielectric::new(1.5))));
+    world.add(ConstantMedium::from_color(boundary2.clone(), 0.0001, Color::new(1.0, 1.0, 1.0)));
+
+    // Earth
+    let emat = Arc::new(Lambertian::new(Arc::new(ImageTexture::new(&"assets/earthmap.jpg".to_string()))));
+    world.add(Sphere::new(Point3::new(400.0, 200.0, 400.0), 100.0, emat));
+    let pertext = Arc::new(NoiseTexture::new(0.2, &mut rng));
+    world.add(Sphere::new(Point3::new(220.0, 280.0, 300.0), 80.0, Arc::new(Lambertian::new(pertext))));
+
+    // Boxes 2
+    let mut boxes2 = HittableList::default();
+    let white = Arc::new(Lambertian::from_color(Color::new(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for _ in 0..ns {
+        boxes2.add(Sphere::new(Point3::random_range(&mut rng, 0.0..165.0), 10.0, white.clone()));
+    }
+
+    world.add(Translate::new(
+        Arc::new(RotateY::new(Arc::new(BvhNode::from_list(&boxes2, &mut rng)), 15.0)),
+        Vec3::new(-100.0, 270.0, 395.0)
+    ));
+
+    // Camera
+    let camera = Camera::new(CameraConfig {
+        aspect_ratio: 1.0,
+        image_width,
+        samples_per_pixel,
+        max_depth,
+        background: Color::zero(),
+
+        vfov: 40,
+        lookfrom: Point3::new(478.0, 278.0, -600.0),
         lookat: Point3::new(278.0, 278.0, 0.0),
         vup: Vec3::new(0.0, 1.0, 0.0),
 
